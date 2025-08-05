3201bf5c9c63bb1d702c4dc600f7fc96b99ee3c5
HARSHA MASANDRAPALYA VANARAJAIAH <52009978+vashihatej@users.noreply.github.com>
2025-08-04 22:50:25 -0400

    Difficulty: Easy Problem: 20-valid-parentheses Language: Pandas Date: 04/08/2025

---
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # stack = []
+        # matching = {")":"(", "]":"[", "}":"{"}
+        # for c in s:
+        #     if c in matching:
+        #         if stack and stack[-1] == matching[c]:
+        #             print(c)
+        #             stack.pop()
+        #         else:
+        #             return False
+        #     else:
+        #         stack.append(c)
+        # return True if not stack else False
+        stack = []
+        matching = {')':'(',"]":"[", "}":"{"}
+        for c in s:
+            if c in matching:
+                if stack and stack[-1]==matching[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return True if not stack else False
+
+
+       
+        
