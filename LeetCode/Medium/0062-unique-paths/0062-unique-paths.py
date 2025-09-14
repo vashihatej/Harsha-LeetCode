bf8ad21443217a5b87d905e013e8eb09b4c0825b
HARSHA MASANDRAPALYA VANARAJAIAH <52009978+vashihatej@users.noreply.github.com>
2025-09-14 14:04:34 -0400

    Difficulty: Medium Problem: 62-unique-paths Language: Pandas Date: 14/09/2025

---
+++ LeetCode/Medium/0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,24 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+            #Recursion
+            # def dfs(i, j):
+                
+            #     if i>=m or j >=n :
+            #         return 0
+            #     if i==m-1 and j==n-1:
+            #         return 1
+            #     return dfs(i+1, j) + dfs(i, j+1)
+            # return dfs(0, 0)
+            
+            #Memozization
+            dp = [[-1] * n for _ in range(m)]
+            def dfs(i, j):
+                if i==(m-1) and j==(n-1):
+                    return 1
+                if i>=m or j >=n :
+                    return 0
+                if dp[i][j]!=-1:
+                    return dp[i][j]
+                dp[i][j]=dfs(i+1, j) + dfs(i, j+1)
+                return  dp[i][j]
+            return dfs(0, 0)
