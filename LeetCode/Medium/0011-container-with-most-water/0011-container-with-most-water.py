d4cad6ac23c41fe3e27024ceea750af74ee363b6
HARSHA MASANDRAPALYA VANARAJAIAH <52009978+vashihatej@users.noreply.github.com>
2025-09-15 23:20:13 -0400

    Difficulty: Medium Problem: 11-container-with-most-water Language: Pandas Date: 15/09/2025

---
+++ LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        # maxarea = 0
+        # l= 0
+        # r=len(height)-1
+        # while l<r:
+        #     area=min(height[l], height[r])*(r-l)
+        #     if area>maxarea:
+        #         maxarea=area
+        #     elif height[l]<height[r]:
+        #         l+=1
+        #     else:
+        #         r-=1
+        # return maxarea
+        l=0
+        r=len(height)-1
+        maxarea=0
+        while l<r:
+            area = min(height[l], height[r]) * (r-l)
+            maxarea = max(maxarea, area)
+            if height[l] < height[r]:
+                l+=1
+            else:
+                r-=1
+        return maxarea
