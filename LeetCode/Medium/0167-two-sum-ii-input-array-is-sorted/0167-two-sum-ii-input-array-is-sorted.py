0d6ac72a0e26b262eaa1f9a4cc61410fe5685d0f
HARSHA MASANDRAPALYA VANARAJAIAH <52009978+vashihatej@users.noreply.github.com>
2025-09-15 22:03:57 -0400

    Difficulty: Medium Problem: 167-two-sum-ii-input-array-is-sorted Language: Pandas Date: 15/09/2025

---
+++ LeetCode/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,34 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # freq ={}
+        # for i in range(0, len(numbers)):
+        #     a = numbers[i]
+        #     diff = target - a
+        #     if diff in freq:
+        #         return [freq[diff]+1, i+1]
+        #     freq[a] = i
+        # start=0
+        # end=len(numbers)-1
+        # while start<end:
+        #     cursum=numbers[start]+numbers[end]
+        #     if cursum<target:
+        #         start+=1
+        #     elif cursum>target:
+        #         end-=1
+        #     else:
+        #         return[start+1, end+1]
+        start=0
+        end=len(numbers)-1
+        while start<end:
+            sum=numbers[start]+numbers[end]
+            if sum < target:
+                start+=1
+            elif sum > target:
+                end-=1
+            else:
+                return [start+1, end+1]
+                
+            
+
+
+        
