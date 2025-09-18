85c44153f7f82b82a122fb0ab85fd21ca9386591
HARSHA MASANDRAPALYA VANARAJAIAH <52009978+vashihatej@users.noreply.github.com>
2025-09-17 21:32:48 -0400

    Difficulty: Easy Problem: 144-binary-tree-preorder-traversal Language: Pandas Date: 17/09/2025

---
+++ LeetCode/Easy/0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # stack=[]
+        # cur = root
+        # res=[]
+        # while cur or stack:
+        #     if cur:
+        #         res.append(cur.val)
+        #         if cur.right:
+        #             stack.append(cur.right)
+        #         cur=cur.left
+        #     else:
+        #         cur=stack.pop()
+        # return res
+        node = root
+        res=[]
+        def dfs(node, res):
+            if not node:
+                return
+            res.append(node.val)
+            dfs(node.left, res)
+            dfs(node.right, res)
+        dfs(node, res)
+        return res
