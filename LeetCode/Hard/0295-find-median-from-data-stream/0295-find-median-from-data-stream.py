f6fadb343b91ec4b76f313b8fd9d12d10b14ddc1
HARSHA MASANDRAPALYA VANARAJAIAH <52009978+vashihatej@users.noreply.github.com>
2025-10-11 12:56:50 -0400

    Difficulty: Hard Problem: 295-find-median-from-data-stream Language: Pandas Date: 11/10/2025

---
+++ LeetCode/Hard/0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -0,0 +1,37 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.small=[]
+        self.large=[]
+        
+    #Remember what ever action we do like push or pop on small heap which is max heap should be multiplied with -1
+    def addNum(self, num: int) -> None:
+        # Push to the max heap and swap with min heap if needed.
+        heapq.heappush(self.small, -1 * num)
+        if (self.small and self.large and (-1 * self.small[0]) > self.large[0]):
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+
+        # Handle uneven size
+        if len(self.small) > len(self.large) + 1:
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        if len(self.large) > len(self.small) + 1:
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -1 * val)
+
+        
+
+    def findMedian(self) -> float:
+        if len(self.small) > len(self.large):
+            return -1 * self.small[0]
+        elif len(self.large) > len(self.small):
+            return  self.large[0]
+        return (-1 * self.small[0] + self.large[0]) / 2
+        
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
