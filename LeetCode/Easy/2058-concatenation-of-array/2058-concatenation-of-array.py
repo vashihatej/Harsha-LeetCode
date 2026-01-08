b03df95c377472b48d97bd3fdcd5df1788c11b55
HARSHA MASANDRAPALYA VANARAJAIAH <52009978+vashihatej@users.noreply.github.com>
2026-01-08 17:19:38 -0500

    Delete old solution - Difficulty: Easy Problem: 2058-concatenation-of-array Language: Pandas Date: 08/01/2026

--- LeetCode/Easy/2058-concatenation-of-array/2058-concatenation-of-array.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def getConcatenation(self, nums: List[int]) -> List[int]:
-        # nums1 = nums.copy()
-        # return nums+nums1
-        ans=nums.extend(nums)
-        return nums
