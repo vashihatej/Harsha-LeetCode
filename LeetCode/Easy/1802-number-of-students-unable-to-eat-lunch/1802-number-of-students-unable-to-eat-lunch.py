ac5d63cc2184bf110355dfb86164772c88e5825d
HARSHA MASANDRAPALYA VANARAJAIAH <52009978+vashihatej@users.noreply.github.com>
2026-01-11 00:26:55 -0500

    Difficulty: Easy Problem: 1802-number-of-students-unable-to-eat-lunch Language: Pandas Date: 11/01/2026

---
+++ LeetCode/Easy/1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,33 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        # n = len(students)
+        # count = Counter(students)
+        # res = n
+
+        # for s in sandwiches:
+        #     if count[s] > 0:
+        #         count[s] -= 1
+        #         res-=1
+        #     else:
+        #         break
+        # return res
+
+
+        n = len(students)
+        q = deque(students)
+
+        res = n
+        for s in sandwiches:
+            cnt=0
+            while cnt<n and s != q[0]:
+                temp = q.popleft()
+                q.append(temp)
+                cnt+=1
+            if s == q[0]:
+                q.popleft()
+                res-=1
+            else:
+                break
+        return res
+
+
