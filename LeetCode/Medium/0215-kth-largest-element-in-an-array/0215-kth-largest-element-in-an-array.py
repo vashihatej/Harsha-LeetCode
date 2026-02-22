a0b7e1e6d76ab3bf5dd68f978927597bb8391510
HARSHA MASANDRAPALYA VANARAJAIAH <52009978+vashihatej@users.noreply.github.com>
2026-02-21 20:40:08 -0500

    Difficulty: Medium Problem: 215-kth-largest-element-in-an-array Language: Pandas Date: 21/02/2026

---
+++ LeetCode/Medium/0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heapq.heapify(nums)
+        while (len(nums)) > k:
+            heapq.heappop(nums)
+        return heapq.heappop(nums)
+        
+# Can also be solved by quick select
