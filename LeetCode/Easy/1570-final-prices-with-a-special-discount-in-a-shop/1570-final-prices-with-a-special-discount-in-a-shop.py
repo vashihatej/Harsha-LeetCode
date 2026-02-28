3cde5bf2f471725f7f799182dbb448bcaac5b180
HARSHA MASANDRAPALYA VANARAJAIAH <52009978+vashihatej@users.noreply.github.com>
2026-02-28 17:54:47 -0500

    Difficulty: Easy Problem: 1570-final-prices-with-a-special-discount-in-a-shop Language: Pandas Date: 28/02/2026

---
+++ LeetCode/Easy/1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,10 @@
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        res=prices[:]
+        stack=[]
+        for i in range(len(prices)):
+            while stack and prices[stack[-1]] >= prices[i]:
+                j=stack.pop()
+                res[j]=prices[j]-prices[i]
+            stack.append(i)
+        return res
